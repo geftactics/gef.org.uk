e586e9a9219de48622ca88cecd69241270f68360
Geoff Kendal <gkendal@williamhill.co.uk>
2024-02-05 16:50:32 +0000

    adding domain name

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -1,7 +1,11 @@
 resource "aws_cloudfront_function" "index_rewrite" {
-  name    = "index-rewrite-${var.environment}"
+  name    = "gef-org-uk-index-rewrite-${var.environment}"
   runtime = "cloudfront-js-2.0"
   comment = "Index.html rewrite"
   publish = true
   code    = file("${path.module}/src/index_rewrite.js")
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
